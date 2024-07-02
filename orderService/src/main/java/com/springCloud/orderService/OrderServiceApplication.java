86d3fc48676028bef877920c025ceed2d89f3b2d
mamtatamma <mamta.tamma@zymr.com>
2024-07-02 17:03:04 +0530

    Implemented Eureka services using resttemplate and loadBalancer.

--- orderService/src/main/java/com/springCloud/orderService/OrderServiceApplication.java
+++ orderService/src/main/java/com/springCloud/orderService/OrderServiceApplication.java
@@ -4,6 +4,7 @@ package com.springCloud.orderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,7 @@ public class OrderServiceApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate()
 	{
 		return new RestTemplate();
