a72c43b6e435c98df821bd20b2764aeb7a9be922
mamtatamma <mamtatamma1@gmail.com>
2024-07-08 15:27:49 +0530

    Implemented Resilience 4j for falut tolerance with fallback methods.

--- orderService/src/main/java/com/springCloud/orderService/controller/OrderController.java
+++ orderService/src/main/java/com/springCloud/orderService/controller/OrderController.java
@@ -4,8 +4,13 @@ import com.springCloud.orderService.entity.Order;
 import com.springCloud.orderService.entity.TransactionRequest;
 import com.springCloud.orderService.entity.TransactionResponse;
 import com.springCloud.orderService.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -15,6 +20,8 @@ public class OrderController {
     private OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name = "orderPaymentCB", fallbackMethod = "orderPaymentFallback")
+//    @Retry(name = "orderPaymentRetry", fallbackMethod = "orderPaymentFallback")
     public TransactionResponse bookOrder(@RequestBody TransactionRequest transactionRequest){
         return orderService.saveOrder(transactionRequest);
     }
@@ -24,4 +31,15 @@ public class OrderController {
         return "Ordered";
     }
 
+    public TransactionResponse orderPaymentFallback(Exception e) {
+        Order order = new Order();
+        order.setName("order1");
+        order.setQty(2);
+        order.setPrice(2000);
+        TransactionResponse transactionResponse = new TransactionResponse();
+        transactionResponse.setOrder(order);
+        transactionResponse.setAmount(2000);
+        transactionResponse.setTransactionId(UUID.randomUUID().toString());
+        transactionResponse.setResponseMessage("fallback order");
+        return transactionResponse;    }
 }
